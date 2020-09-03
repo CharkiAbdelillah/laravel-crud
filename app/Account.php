43ef130e93f4d7d4e5ed5b4ae6cdb7b565933b3c
CharkiAbdelillah <charkiabdillah@gmail.com>
2020-09-03 17:11:08 +0100

    a

--- app/Account.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace kk;
-class Accountant{
-
-function findProfil($amount){
- $profil=10;
-  return $amount*$profil/100;
-
-}}
--- tests/Unit/MathTest.php
+++ tests/Unit/MathTest.php
@@ -4,8 +4,7 @@ namespace Tests\Unit;
 use App\Math;
 use PHPUnit\Framework\TestCase;
 
-class MathTest extends TestCase
-{
+class MathTest extends TestCase{
     public function testFibonacci() {
         $ar=Math::fibonacci(10);
         $this->assertEquals(100,$ar);
