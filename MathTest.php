8598ef352d1f9269476975433adad3831c20825e
CharkiAbdelillah <charkiabdillah@gmail.com>
2020-09-03 16:57:02 +0100

    test Math

---
+++ MathTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+use App\Math;
+use PHPUnit\Framework\TestCase;
+
+class MathTest extends TestCase
+{
+    public function testFibonacci() {
+        $ar=Math::fibonacci(10);
+        $this->assertEquals(100,$ar);
+    }
+}
